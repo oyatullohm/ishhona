7b67d7538fa2b36ecb06874268fe9e11b86705f1
oyatullohm <marziyahoni@gmail.com>
2025-10-24 17:39:43 +0500

    f

--- bot/keyboards/admin_kb.py
+++ bot/keyboards/admin_kb.py
@@ -368,6 +368,7 @@ class IncomeState(StatesGroup):
     price = State()
     client= State()
     payment_type = State()
+    payment_type_ = State()
     cash = State()
 
 class WorkerMoneyState(StatesGroup):
